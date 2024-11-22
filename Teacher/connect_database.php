671bdf04954908c0855442353dfc50097bd80563
sojun15 <sojun2115@cseku.ac.bd>
2024-11-22 20:52:11 +0600

    today i will do controller and database connection part

--- Teacher/connect_database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'tms';
-$connect_db = new mysqli($host, $user, $password, $database);
-if ($connect_db->connect_error) {
-    echo "fail to connect database" . $connect_db->connect_error;
-}
--- student/connection-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$db = 'tms';
-$conn = new mysqli($host, $user, $password, $db);
-if ($conn->connect_error) {
-    echo "fail to connect db" . $conn->connect_error;
-}
